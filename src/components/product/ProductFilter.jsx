9cfe40196bc92f21ed1eb2b59eb8f0bbcd0bc1cc
nguyenkhangdev <nguyenkhangdev@gmail.com>
2025-07-17 01:44:56 +0700

    feat: update filter

--- src/components/product/ProductFilter.jsx
+++ src/components/product/ProductFilter.jsx
@@ -10,22 +10,29 @@ export default function ProductFilter({
   onFilterChange,
 }) {
   const [priceRange, setPriceRange] = useState("");
+  const [category, setCategory] = useState("");
 
   // Gọi khi filter thay đổi
   useEffect(() => {
-    onFilterChange({ searchTerm, priceRange });
-  }, [searchTerm, priceRange, onFilterChange]);
+    onFilterChange({ searchTerm, category, priceRange });
+  }, [searchTerm, category, priceRange, onFilterChange]);
+
+  const handleCategory = (selected) => {
+    setCategory(selected);
+  };
 
   const handlePriceChange = (selected) => {
     setPriceRange(selected);
   };
 
   const handleRemoveFilter = (type) => {
+    if (type === "category") setCategory("");
     if (type === "price") setPriceRange("");
     if (type === "search") setSearchTerm("");
   };
 
   const handleClearAll = () => {
+    setCategory("");
     setPriceRange("");
     setSearchTerm("");
   };
@@ -40,20 +47,20 @@ export default function ProductFilter({
       />
 
       <div className="relative z-10 text-center">
-        <h1 className="text-3xl md:text-5xl font-extrabold">
-          TÌM GIÁO VIÊN TIẾNG ANH TỐT NHẤT
+        <h1 className="text-3xl md:text-4xl lg:text-5xl font-extrabold">
+          KHÁM PHÁ SẢN PHẨM TIẾNG ANH CHẤT LƯỢNG
         </h1>
         <p className="mt-2 text-lg md:text-xl">
-          Tìm giáo viên tiếng Anh tốt nhất trên toàn cầu
+          Chọn lựa khóa học, tài liệu và giáo trình phù hợp với bạn
         </p>
       </div>
 
       {/* Search + Filters */}
       <div className="relative z-10 mt-12 flex justify-center">
-        <div className="bg-white shadow-lg rounded-md p-4 flex flex-wrap md:flex-nowrap items-center gap-3 md:gap-4 w-[90%] max-w-5xl">
+        <div className="grid md:grid-cols-6 sm:grid-cols-3 gap-3 md:gap-0 bg-white shadow-lg rounded-md p-4 items-center w-[90%] max-w-5xl">
           {/* Search Input */}
-          <div className="flex items-center border px-3 py-3 rounded w-full md:w-auto flex-1">
-            <Search className="text-gray-400 h-5 w-5 mr-2" />
+          <div className="md:col-span-3 sm:col-span-3 flex items-center border px-3 py-3 rounded w-full md:w-auto flex-1 h-full">
+            <Search className="text-gray-400 h-5 w-5 mr-2 " />
             <input
               type="text"
               placeholder="Bạn muốn học gì?"
@@ -63,21 +70,32 @@ export default function ProductFilter({
             />
           </div>
 
-          {/* Price Dropdown */}
-          <DropdownProps
-            label="Giá"
-            items={["< 500K", "500K – 1 triệu", "> 1 triệu"]}
-            onSelect={handlePriceChange}
-          />
+          <div className="flex flex-row  md:col-span-2 sm:col-span-2">
+            {/* Category Dropdown */}
+            <div className="w-1/2 h-full">
+              <DropdownProps
+                label="Loại"
+                items={["Lớp học trực tuyến", "Giáo trình", "Tài liệu"]}
+                onSelect={handleCategory}
+              />
+            </div>
+            <div className="w-1/2 h-full">
+              {/* Price Dropdown */}
+              <DropdownProps
+                label="Giá"
+                items={["< 500K", "500K – 1 triệu", "> 1 triệu"]}
+                onSelect={handlePriceChange}
+              />
+            </div>
+          </div>
 
-          <Button className="bg-green-500 hover:bg-green-600 rounded-full px-6 md:px-8 md:py-5">
-            Tìm giáo viên
+          <Button className="sm:ml-3 sm:font-bold sm:text-lg bg-green-500 hover:bg-green-600 rounded-full h-full px-6 md:px-8 md:py-3 md:col-span-1">
+            Tìm sản phẩm
           </Button>
         </div>
       </div>
 
-      {/* Trạn thái filter */}
-
+      {/* Trạng thái filter */}
       <div className="relative z-10 my-10 flex justify-center">
         <div className="w-[90%] max-w-5xl flex items-center gap-2 text-sm md:text-base text-gray-900 flex-wrap">
           <span className="font-semibold uppercase text-sm text-black">
@@ -89,6 +107,18 @@ export default function ProductFilter({
               {searchTerm}
               <button
                 onClick={() => handleRemoveFilter("search")}
+                className="text-gray-500 hover:text-black"
+              >
+                <X className="h-4 w-4" />
+              </button>
+            </div>
+          )}
+
+          {category && (
+            <div className="bg-gray-100 px-3 py-1 rounded-full flex items-center gap-2">
+              {category}
+              <button
+                onClick={() => handleRemoveFilter("category")}
                 className="text-gray-500 hover:text-black"
               >
                 <X className="h-4 w-4" />
--- src/components/product/ProductsList.jsx
+++ src/components/product/ProductsList.jsx
@@ -60,7 +60,7 @@ export default function ProductsList({ products, setProducts }) {
       });
   };
   return (
-    <div className="max-w-[1100px] mx-auto p-6 grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-6">
+    <div className="max-w-[1100px] mx-auto p-6 pt-0 grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-6">
       {products.map((product) => (
         <ProductCard
           key={product.id}
